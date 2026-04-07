7a2efc5299267d6feb73a54c28b0bcd837f5eb97
amayorga-pixel <amayorga@utem.cl>
2026-04-06 23:42:26 -0400

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -62,18 +62,15 @@ def mayor_defensa(df):
 
 #Retorna el pokemon con la menor velocidad
 def menor_velocidad(df):
-
     indice = df["Velocidad"].idxmin()
     return df.loc[indice, "Nombre"]
 
 #Retorna la cantidad de pokemones que tienen dos tipos 
 def dos_tipos(df):
-
     return len(df[df["Tipo 2"].notna()])
 
 #Retorna el rango de los ps
 def calcular_rango(df):
-
     return df["PS"].max() - df["PS"].min()
 
 #Retorna la desviacion de los PS
@@ -99,7 +96,6 @@ def grafico_dispersion_AtaqueYvelocidad(df):
 
 #Diegrama de caja de los PS
 def boxplot_PS(df):
-    
     df.boxplot(column ="PS" , by="Tipo 1")
     plt.title("..")
     plt.suptitle("")
@@ -112,7 +108,6 @@ def boxplot_PS(df):
 def diagrama_violin_defensa(df):
     plt.figure(figsize=(8,6))
     sns.violinplot(y=df["Defensa"])
-
     plt.title("Distribucion de la defensa")
     plt.xlabel("Defensa")
     plt.show()
@@ -120,7 +115,6 @@ def diagrama_violin_defensa(df):
 #5. MANIPULACION DE DATOS
 #Crear la columna de poder total
 def crear_PoderTotal(df):
-
     df["Poder total"] = df["Ataque"] + df["Defensa"] + df["Velocidad"] + df["PS"]
     df.to_csv("pokemon_primera_gen_actualizado.csv", index=False)
     return df
@@ -128,33 +122,27 @@ def crear_PoderTotal(df):
 #Ordenar la columna por poder de mayor a menor
 def Ordenar(df):
     df = df.sort_values(by = "Poder total", ascending = False)
-
     return df
 
 #6.MANIPULACION DE DATOS
 #Calcular el promedio, mediana y desviacion estandar del ataque por cada tipo de pokemon
 def calcular_tipo(df):
-
     resultado = df.groupby("Tipo 1")["Ataque"].agg(["mean" , "median" , "std"]) #agg para sacar varias operaciones a la vez
-
     return resultado
+
 #Que tipo tiene el mayor promedio de velocidad
 def mayorPromedio_velocidad(df):
     resultado = df.groupby("Tipo 1")["Velocidad"].mean().idxmax()
-
-
     return resultado
+
 #Pokémon con mayor y menor PS
 def MayoryMenor_PS(df):
-    
     idx_max = df.groupby("Tipo 1")["PS"].idxmax()#Se obtienen los índices mayores de PS para cada tipo principal
     idx_min = df.groupby("Tipo 1")["PS"].idxmin()#Se obtienen los índices minimos de PS para cada tipo principal
 #Se crean dos tablas con los datos pedidos, renombrando columnas para que sean mas claras
     maximos = df.loc[idx_max, ["Tipo 1", "Nombre", "PS"]].rename(columns={"Nombre": "Pokémon_Max", "PS": "PS_Max"})
     minimos = df.loc[idx_min, ["Tipo 1", "Nombre", "PS"]].rename(columns={"Nombre": "Pokémon_Min", "PS": "PS_Min"})
-    
     resultado = pd.merge(maximos, minimos, on="Tipo 1")#Unión de tablas
-
     return resultado
 
 #7. Análisis exploratorio 
@@ -176,17 +164,14 @@ def dispersion(df):
 #Identificar outliers en ataque y PS utilizando boxplots
 def identificar_outliers(df):
     plt.figure(figsize=(10, 5))
-
     # Boxplot para Ataque
     plt.subplot(1, 2, 1)
     sns.boxplot(y=df["Ataque"], color="skyblue")
     plt.title("Outliers en Ataque")
-
     # Boxplot para PS
     plt.subplot(1, 2, 2)
     sns.boxplot(y=df["PS"], color="salmon")
     plt.title("Outliers en PS")
-
     plt.tight_layout()
     plt.show()
 
